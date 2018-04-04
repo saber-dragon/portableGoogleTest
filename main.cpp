707582a7aead3e65311d36435687c9180ebca6d4
Long Gong <saber.fate.dragon@gmail.com>
2018-04-03 20:36:16 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <tuple>
+
+#include <gtest/gtest.h>
+
+using namespace std;
+
+
+tuple<int, double, bool, string> foo(){
+    return {1, 1.0, false, "hello world"};
+}
+
+
+
+TEST(HelloWorldTest, ReturnATuple){
+    auto t = foo();
+    EXPECT_EQ(1, get<0>(t));
+    EXPECT_EQ(1.0, get<1>(t));
+    EXPECT_FALSE(get<2>(t));
+    EXPECT_STREQ("hello world", get<3>(t).c_str());
+}
+
+int main(int argc, char* argv[])
+{
+   ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
